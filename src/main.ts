408cd551e95bb6ae9fb558c5d58a6daacc3d6ad9
Vinh Phuc <killermh1352@gmail.com>
2023-12-10 07:45:51 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from 'middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
