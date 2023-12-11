7ebff5cebbfbda5b0bf34ad3a6b802c43c296d2a
Vinh Phuc <killermh1352@gmail.com>
2023-12-11 21:41:40 +0700

    fix uuid

--- src/modules/course/resources/dto/upsertInvitation.dto.ts
+++ src/modules/course/resources/dto/upsertInvitation.dto.ts
@@ -1,5 +1,4 @@
 import { IsEmail, IsString } from 'class-validator';
-import { v4 as uuidv4 } from 'uuid';
 import { defaultValue } from 'utils/decorator/parameters';
 import { InvitationState } from 'utils/prisma/client';
 import { UserCourseRole } from 'utils/prisma/client';
@@ -10,8 +9,7 @@ export class BatchInvitationDto {
 }
 
 export class CreateInvitationDto extends BatchInvitationDto {
-  @defaultValue(uuidv4())
-  id: string;
+  id?: string;
   @defaultValue(InvitationState.PROCESSING)
   state: InvitationState;
   @defaultValue(UserCourseRole.STUDENT)
